9ec8a94c80f5fa49b8486eed6b200ea8894c50f9
Sergio <e.livingappsupport1.tid@telefonica.com>
2021-09-04 22:41:05 +0200

    blog starter

--- components/date.js
+++ components/date.js
@@ -1,9 +1,11 @@
-import { isValid, parseISO, format } from 'date-fns'
+import { isValid, parseISO, format } from 'date-fns';
+import { es } from 'date-fns/locale';
+import capitalize from 'lodash.capitalize';
 
 export default function Date({ dateString }) {
-  if (!isValid(parseISO(dateString))) {
-    return 'No date'
-  }
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+    if (!isValid(parseISO(dateString))) {
+        return 'No date';
+    }
+    const date = parseISO(dateString);
+    return <time dateTime={dateString}>{capitalize(format(date, 'LLLL	d, yyyy', { locale: es }))}</time>;
 }
--- components/intro.js
+++ components/intro.js
@@ -1,28 +1,12 @@
-import { CMS_NAME, CMS_URL } from '../lib/constants'
+import { CMS_NAME, CMS_URL } from '../lib/constants';
 
 export default function Intro() {
-  return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and{' '}
-        <a
-          href={CMS_URL}
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Sanity.io
-        </a>
-        .
-      </h4>
-    </section>
-  )
+    return (
+        <section className='flex-col md:flex-row flex items-center mt-16 mb-16 md:mb-12 align-bottom'>
+            <h1 className='text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8'>Trading Blog.</h1>
+            <h4 className='text-center md:text-left text-lg mb-4 md:pl-8 self-end'>
+                Aprende la manera de hacer trading de forma consciente y efectiva.{' '}
+            </h4>
+        </section>
+    );
 }
