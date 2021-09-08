cc3152a4444c21a726e33b1e2207666b72177b42
Sergio <e.livingappsupport1.tid@telefonica.com>
2021-09-08 16:20:18 +0200

    lazy load

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,17 +1,21 @@
 import cn from 'classnames';
 import Link from 'next/link';
 import { imageBuilder } from '../lib/sanity';
+import LazyLoad from 'react-lazyload';
 
-export default function CoverImage({ title, imageObject, slug }) {
+export default function CoverImage({ title, imageObject, slug, hero }) {
     const image = (
         <img
-            width={1240}
+            width={hero ? 1496 : 1240}
             height={540}
             alt={`Imágen de portada de ${title}`}
             className={cn('shadow-small', {
                 'hover:shadow-medium transition-shadow duration-200': slug,
             })}
-            src={imageBuilder(imageObject).width(1240).height(540).url()}
+            src={imageBuilder(imageObject)
+                .width(hero ? 1496 : 1240)
+                .height(540)
+                .url()}
         />
     );
 
@@ -19,7 +23,11 @@ export default function CoverImage({ title, imageObject, slug }) {
         <div className='-mx-5 sm:mx-0'>
             {slug ? (
                 <Link as={`/posts/${slug}`} href='/posts/[slug]'>
-                    <a aria-label={title}>{image}</a>
+                    <a aria-label={title}>
+                        <LazyLoad height={540} offset={100}>
+                            {image}
+                        </LazyLoad>
+                    </a>
                 </Link>
             ) : (
                 image
