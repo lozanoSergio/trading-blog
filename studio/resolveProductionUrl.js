d0fb94d5a014644b56472dcff9e3e586b1bb08e2
Sergio <e.livingappsupport1.tid@telefonica.com>
2021-09-13 17:34:46 +0200

    resolve preview token

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -1,8 +1,8 @@
 export default function resolveProductionUrl(doc) {
-    const previewUrl = new URL(process.env.NEXT_PUBLIC_URL);
+    const previewUrl = new URL(process.env.VERCEL_URL ?? 'http://localhost:3000');
 
     previewUrl.pathname = `/api/preview`;
-    previewUrl.searchParams.append(`secret`, process.env.SANITY_PREVIEW_SECRET);
+    previewUrl.searchParams.append(`secret`, '5otkU30CV9UiX0M35g1MzEC4pQtgHSCL');
     previewUrl.searchParams.append(`slug`, doc?.slug?.current ?? `/`);
 
     return previewUrl.toString();
