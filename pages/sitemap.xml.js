f91fd455b3b57ea5def11a71d257352e9bab2842
Sergio <e.livingappsupport1.tid@telefonica.com>
2021-10-04 22:09:44 +0200

    sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -23,8 +23,6 @@ export const getServerSideProps = async ({ res }) => {
 
     const postsMeta = await getAllPostsForHome();
 
-    console.log(postsMeta[0]);
-
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${staticPages
